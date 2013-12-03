4c477d0e7d0e11b50b03abd31b41afefb3c28724
pgayane <pgayane@gmail.com>
2013-12-03 14:50:33 -0500

    command line options improved
    - now search query can contain author and title info similtaneously

--- dblp.py
+++ dblp.py
@@ -1,8 +1,11 @@
 import requests
 import argparse
 
-def get_papers(query):
-  payload = {'q':query,  'format':'json'}
+def get_papers(query, top):
+  payload = {'q':query, 'format':'json'}
+  if top != 0:
+	payload['h'] = top
+
   r = requests.get("http://www.dblp.org/search/api/", params=payload)
   result = r.json()
 
@@ -21,22 +24,37 @@ def get_papers(query):
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Searches DBLP')
-  parser.add_argument('query', help='Search query')
-  parser.add_argument('--title', action='store_true', help='Print title')
-  parser.add_argument('--author', action='store_true', help='Print author')
-  parser.add_argument('--top', type=int, help='Only print top n match')
+	parser = argparse.ArgumentParser(description='Searches DBLP')
+	parser.add_argument('query', nargs = "?", help='Search query')
+	parser.add_argument('--title', help='Print title')
+	parser.add_argument('--author', help='Print author')
+	parser.add_argument('--top', type=int, default = 0, help='Only print top n match')
 
-  args = parser.parse_args()
+	args = parser.parse_args()
 
-  results = get_papers(args.query)
-  if args.top:
-    results = results[:args.top]
+	query = ''
+	if args.query:
+		query = args.query
+		
+	if args.author:
+		if query:
+			query += '+'
+		query += 'ce:author:'+args.author.lower().replace(' ', '_')
+	
+	if args.title:
+		if query:
+			query += '+'
+		query += args.title
+		
+	results = get_papers(query, args.top)
+  
+	for paper in results:
+		print 'Title:', paper['title']
+		print 'Author:', ', '.join(paper['author'])
+		print 'Year:', paper['year']
+		print 'DOI:', paper['doi']
+		print 'URL:', paper['url']
+		print '\n'
 
-  for paper in results:
-    print 'Title:', paper['title']
-    print 'Author:', ', '.join(paper['author'])
-    print 'Year:', paper['year']
-    print 'DOI:', paper['doi']
-    print 'URL:', paper['url']
-    print '\n'
+	
+	
